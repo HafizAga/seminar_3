4684af4d07d451b099b5b8d921319ed9e591da40
HafiAga <ulguiorra2@gmail.com>
2022-11-28 11:55:36 +0300

    Work

---
+++ Ex001/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите ваше число:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите ваше число:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+if(x > 0 && y > 0)
+
+Console.WriteLine("первая четверть");
---
+++ Ex003/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите номер четверти ");
+int quart = Convert.ToInt32(Console.ReadLine());
+if (quart == 1)
+{
+    Console.WriteLine("Rande x > 0 && y > 0");
+}
+if (quart == 2)
+{
+    Console.WriteLine("Rande x > 0 && y < 0");
+}
+if (quart == 3)
+{
+    Console.WriteLine("Rande x < 0 && y > 0");
+}
+if (quart == 4)
+{
+    Console.WriteLine("Rande x < 0 && y < 0");
+}
+
